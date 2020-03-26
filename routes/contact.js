35d0aa78471587158cd0999a9d58dbecc99ceb6c
ArthurGoupil <arthurgoupil@gmail.com>
2020-03-26 12:22:37 +0100

    add mailgun package

--- routes/contact.js
+++ routes/contact.js
@@ -5,17 +5,20 @@ let mailgun = require("mailgun-js")({
   domain: process.env.MAILGUN_DOMAIN
 });
 
-router.post("contact/more-infos", (req, res) => {
+router.post("/contact/more-infos", (req, res) => {
   try {
     const { lastName, name, email, tel, message } = req.fields;
+    const mailName = `${name || lastName ? " " : ""}${name ? name : ""}${
+      lastName ? lastName : ""
+    }`;
     const data = {
-      from: "Excited User <me@samples.mailgun.org>",
-      to: "arthurgoupil@gmail.com",
-      subject: "Hello",
-      text: "Testing some Mailgun awesomeness!"
+      from: `Gainz Art <me@${process.env.MAILGUN_DOMAIN}>`,
+      to: `arthurgoupil@gmail.com`,
+      subject: "Nous avons bien reçu votre demande. Message well received.",
+      text: `Bonjour${mailName},\n\nMerci de votre intérêt pour le travail de Gainz.\nNous avons bien reçu votre demande et reviendrons vers vous dans les plus brefs délais.\n\nÀ très bientôt,\nL'équipe Gainz.\n\n\nHi${mailName},\n\nThanks for your interest in Gainz work.\nWe've well received your message and will get back to you shortly.\n\nBest,\nGainz Team.`
     };
     mailgun.messages().send(data, (error, body) => {
-      res.status(200).json({ message: "Email has been sent." });
+      res.status(200).json({ message: "body : " + body + "error : " + error });
     });
   } catch (e) {
     res.status(400).json({ error: e.message });
