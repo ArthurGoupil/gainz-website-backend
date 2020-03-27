f7cbbe790b530cf225c257a9d40d9cc5605bb0b2
ArthurGoupil <arthurgoupil@gmail.com>
2020-03-27 18:39:54 +0100

    fix contact routes

--- routes/contact.js
+++ routes/contact.js
@@ -1,31 +1,63 @@
 const express = require("express");
 const router = express.Router();
 const mailgun = require("mailgun-js");
+const User = require("../models/User");
 
-router.post("/contact/more-infos", (req, res) => {
+router.post("/contact/more-infos", async (req, res) => {
   const mg = mailgun({
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_DOMAIN,
     host: "api.eu.mailgun.net"
   });
   try {
-    const { lastName, name, email, tel, message } = req.fields;
-    const mailName = `${name || lastName ? " " : ""}${name ? name : ""}${name &&
-      lastName &&
-      " "}${lastName ? lastName : ""}`;
-    const data = {
+    const {
+      lastName,
+      name,
+      email,
+      tel,
+      message,
+      artType,
+      artName
+    } = req.fields;
+    const mailName = `${name ? " " : ""}${name ? name : ""}`;
+    const users = await User.find({ role: "admin" });
+    let usersEmail = "";
+    let userResponse = {};
+    let adminResponse = {};
+
+    users.forEach((user, index) => {
+      if (index === users.length - 1) {
+        usersEmail += user.email;
+      } else {
+        usersEmail += user.email + ", ";
+      }
+    });
+
+    const dataUser = {
       from: `Gainz Art <contact@gainz-art.com>`,
       to: email,
       subject: "Nous avons bien reçu votre demande. Message well received.",
-      html: `<img
-      src="https://apicms.thestar.com.my/uploads/images/2020/02/27/578706.jpg"
-      alt="cat"
-      style="width:100%;"
-    /><br><b>Bonjour</b>${mailName},<br><br>Merci de votre intérêt pour le travail de Gainz.<br>Nous avons bien reçu votre demande et reviendrons vers vous dans les plus brefs délais.<br><br>À très bientôt,<br>L'équipe Gainz.<br><br><br><i>Hi${mailName},<br><br>Thanks for your interest in Gainz work.<br>We've well received your message and will get back to you shortly.<br><br>Best,<br>Gainz Team.</i>`
+      html: `Bonjour${mailName},<br><br>Merci de votre intérêt pour le travail de Gainz.<br>Nous avons bien reçu votre demande et reviendrons vers vous dans les plus brefs délais.<br><br>À très bientôt,<br>L'équipe Gainz.<br><br><br><i>Hi${mailName},<br><br>Thanks for your interest in Gainz work.<br>We've well received your message and will get back to you shortly.<br><br>Best,<br>Gainz Team.</i>`
     };
-    mg.messages().send(data, (error, body) => {
-      res.status(200).json({ body, error });
+    const dataAdmin = {
+      from: `Gainz Art <contact@gainz-art.com>`,
+      to: usersEmail,
+      subject: "Gainz Art - Nouvelle demande.",
+      html: `Salut chouchou,<br><br>Quelqu'un désire obtenir quelques informations.<br><br>La demande porte sur <b>${artName}</b> (${artType})<br><br><b>Détails :</b><br><u>Nom</u> : ${
+        lastName ? lastName : "Non renseigné"
+      }<br><u>Prénom</u> : ${
+        name ? name : "Non renseigné"
+      }<br><u>Email</u> : ${email}<br><u>Téléphone</u> : ${
+        tel ? tel : "Non renseigné"
+      }<br><u>Message</u> : ${message ? message : "Non renseigné"}`
+    };
+    await mg.messages().send(dataAdmin, (error, body) => {
+      console.log({ body, error });
     });
+    await mg.messages().send(dataUser, (error, body) => {
+      console.log({ body, error });
+    });
+    res.status(200).json({ message: "Email sent to user and admins." });
   } catch (e) {
     res.status(400).json({ error: e.message });
   }
