83aac0b9186d64b509cbac99e33365f045e4d099
ArthurGoupil <arthurgoupil@gmail.com>
2020-05-26 16:58:50 +0200

    add signin route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
+const User = require('../models/User');
 
-const SHA256 = require("crypto-js/sha256");
-const encBase64 = require("crypto-js/enc-base64");
-const uid2 = require("uid2");
+const SHA256 = require('crypto-js/sha256');
+const encBase64 = require('crypto-js/enc-base64');
+const uid2 = require('uid2');
 
-router.post("/users/signup", async (req, res) => {
+router.post('/users/signup', async (req, res) => {
   try {
     const { email, password } = req.fields;
     const token = uid2(64);
@@ -17,7 +17,7 @@ router.post("/users/signup", async (req, res) => {
       email,
       token,
       salt,
-      hash
+      hash,
     });
 
     await user.save();
@@ -25,10 +25,32 @@ router.post("/users/signup", async (req, res) => {
     return res.status(200).json({
       _id: user._id,
       email: user.email,
-      token: user.token
+      token: user.token,
     });
   } catch (e) {
     res.status(400).json({ error: e.message });
+  }
+});
+
+router.post('/users/signin', async (req, res) => {
+  const { email, password } = req.fields;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      if (SHA256(password + user.salt).toString(encBase64) === user.hash) {
+        return res.json({
+          _id: user._id,
+          email: user.email,
+          token: user.token,
+        });
+      } else {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    } else {
+      return res.status(400).json('User not found');
+    }
+  } catch (e) {
+    return res.status(400).json({ message: 'An error occurred' });
   }
 });
 
